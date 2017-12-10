d7969f6fec83260ed60ee6e66575d818a893cc4f
1510 <1510973267@qq.com>
2017-12-10 18:55:43 +0800

    2017年12月10日 18:55:40

--- config/devEvn/xampp.js
+++ config/devEvn/xampp.js
@@ -1,12 +1,12 @@
 'xampp'搭建本地服务器 
-  修改服务器根目录指向'httpd.conf'文件
+  'xampp/apache/conf/httpd.conf',修改服务器根目录指向 
     DocumentRoot "E:/project/"                             1
     <Directory "E:/project/">                              2
       Options Indexes FollowSymLinks Includes ExecCGI
       AllowOverride All
       Require all granted
     </Directory>
-  虚拟主机的配置'httpd-vhosts.conf'文件 
+  'xampp/apache/conf/extra/httpd-vhosts.conf',虚拟主机的配置 
     <VirtualHost *:80>                                     
       ServerAdmin webmaster@dummy-host2.example.com
       DocumentRoot "E:/project/"            3
@@ -14,7 +14,9 @@
       ErrorLog "logs/dummy-host2.example.com-error.log"
       CustomLog "logs/dummy-host2.example.com-access.log" common
     </VirtualHost>
-  设置本地 Hosts
+  'C:/Windows/System32/drivers/etc/hosts',本地ip代理 
     127.0.0.1    project.localtst.com                      5
     // 127.0.0.2  上文配置虚拟主机时 VirtualHost 的回送 IP
   共修改'3'个文件'5'个位置
+
+

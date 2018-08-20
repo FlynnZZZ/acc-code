a018eab7226ec19f4e81209f46e9d0415ee9dc49
fanshl <1356007565@qq.com>
2018-08-20 23:40:40 +0800

    2018年8月20日 23:38:02

---
+++ 9Kits/ToolsPages/pages/course/data.js
@@ -0,0 +1,25 @@
+var _data = {
+  JavaScript: {
+    ES6: 'http://es6.ruanyifeng.com/#README'
+    ,Vue: 'http://es6.ruanyifeng.com/#README'
+  }
+  ,PHP: {
+    PHP中文网: 'http://www.php.cn/'
+  }
+  ,'框架/库': {
+    CocosCreator: 'http://www.cocos.com/docs/creator/index.html'
+  }
+  ,工具: {
+    Webpack: 'https://webpack.docschina.org/guides/'
+  }
+  ,综合: {
+    MDN: 'https://developer.mozilla.org/zh-CN/'
+    ,菜鸟教程: 'http://www.runoob.com/'
+  }
+}
+
+
+
+
+
+
---
+++ 9Kits/ToolsPages/pages/docs/data.js
@@ -0,0 +1,12 @@
+var _data = {
+  JavaScript: {
+    jQuery中文: 'https://www.jquery123.com/'
+    ,zepto中文: 'https://zeptojs.bootcss.com/'
+    ,Vue中文: 'https://cn.vuejs.org/v2/api/'
+    ,'vue-router2中文': 'https://router.vuejs.org/zh-cn/'
+    ,vuex中文: 'https://vuex.vuejs.org/zh-cn/'
+  }
+}
+
+
+
